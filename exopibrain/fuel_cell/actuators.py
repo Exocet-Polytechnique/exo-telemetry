a0222d7e52272287c3868d0c4489991495bbc06a
efondere <97473864+efondere@users.noreply.github.com>
2024-01-11 10:17:42 -0500

    Move project files and remove exopibrain

--- exopibrain/fuel_cell/actuators.py
+++
@@ -1,5 +0,0 @@
-class Actuator(object):
-    def open_valve(self):
-        pass
-    def close_valve(self):
-        pass
--- exopibrain/fuel_cell/precharge.py
+++
@@ -1,7 +0,0 @@
-class Precharge(object):
-    """
-    The precharge procedure is yet to
-    be implemented once we recieve and install the 
-    appropriate components.    
-    """
-    pass
